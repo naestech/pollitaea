b4453d05440b6b1edf6a3d03e6c570a698cd1cb6
Samuel Adetunji <guysnacho2@gmail.com>
2023-08-20 21:21:48 -0500

    Add home page and temporary login skip

---
+++ Pollitaea/app/screens/HomeScreen.tsx
@@ -0,0 +1,23 @@
+import { Screen, Text } from "app/components"
+import { AppStackScreenProps } from "app/navigators"
+import { useSafeAreaInsetsStyle } from "app/utils/useSafeAreaInsetsStyle"
+import { observer } from "mobx-react-lite"
+import React, { FC } from "react"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "app/models"
+
+interface HomeScreenProps extends AppStackScreenProps<"Home"> {}
+
+export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen() {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  const $containerInsets = useSafeAreaInsetsStyle(["top", "left", "right"])
+  return (
+    <Screen style={$containerInsets} preset="scroll">
+      <Text text="Home" />
+    </Screen>
+  )
+})
