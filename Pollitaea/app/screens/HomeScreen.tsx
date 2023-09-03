3ca5dafdc5ca84f703c07ccf47834867a95b86bf
Samuel Adetunji <guysnacho2@gmail.com>
2023-09-03 15:42:59 -0500

    Add navbar

--- Pollitaea/app/screens/HomeScreen.tsx
+++ Pollitaea/app/screens/HomeScreen.tsx
@@ -1,18 +1,16 @@
-import { Home, Newspaper, User2, Vote } from "@tamagui/lucide-icons"
 import { Nav, Text } from "app/components"
 import { AppStackScreenProps } from "app/navigators"
 import { useSafeAreaInsetsStyle } from "app/utils/useSafeAreaInsetsStyle"
 import { observer } from "mobx-react-lite"
 import React, { FC } from "react"
-import { Platform } from "react-native"
-import { Separator, Tabs, View, YStack, getTokens } from "tamagui"
+import { View, YStack } from "tamagui"
 
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "app/models"
 
 interface HomeScreenProps extends AppStackScreenProps<"Home"> {}
 
-export const HomeScreen: FC<HomeScreenProps> = observer((props: HomeScreenProps) => {
+export const HomeScreen: FC<HomeScreenProps> = observer(({ route, navigation }) => {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
   const containerStyle = useSafeAreaInsetsStyle(["top", "left", "right"])
@@ -21,10 +19,11 @@ export const HomeScreen: FC<HomeScreenProps> = observer((props: HomeScreenProps)
   // const navigation = useNavigation()
   return (
     <View style={containerStyle} height="100%">
-      <YStack>
-        <Text text="Home" />
-      </YStack>
-      <Nav navProps={props} />
+      <Nav navigation={navigation} route={route}>
+        <YStack>
+          <Text text="Home" />
+        </YStack>
+      </Nav>
     </View>
   )
 })
