baf9186c8e09e36604b8bc17c40bdbbe5f464cc2
Samuel Adetunji <guysnacho2@gmail.com>
2023-09-18 04:17:56 -0500

    Add profile image and field

--- Pollitaea/app/screens/ProfileScreen.tsx
+++ Pollitaea/app/screens/ProfileScreen.tsx
@@ -1,21 +1,51 @@
 import { Nav, Text } from "app/components"
+import { Profile } from "app/config/schema"
+import { useStores } from "app/models"
 import { AppStackScreenProps } from "app/navigators"
+import { supabase } from "app/utils/supabaseClient"
 import { useSafeAreaInsetsStyle } from "app/utils/useSafeAreaInsetsStyle"
 import { observer } from "mobx-react-lite"
-import React, { FC } from "react"
-import { YStack } from "tamagui"
+import React, { FC, useEffect, useState } from "react"
+import { Image, YStack } from "tamagui"
 
 interface ProfileScreenProps extends AppStackScreenProps<"Profile"> {}
 
 export const ProfileScreen: FC<ProfileScreenProps> = observer(({ route, navigation }) => {
   // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const store = useStores()
+  const [profile, setProfile] = useState<Profile>()
   const containerStyle = useSafeAreaInsetsStyle(["top", "left", "right"])
+
+  useEffect(() => {
+    // if (!store.user.profilePic)
+    supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", store.user.id)
+      .single()
+      .then(({ data, error }) => {
+        console.log(data, error)
+        if (data) {
+          setProfile(data)
+          store.user.setProfilePic(data.avatar_url)
+        }
+      })
+  }, [])
 
   return (
     <Nav navigation={navigation} route={route}>
-      <YStack style={containerStyle}>
-        <Text text="Profile" />
+      <YStack style={containerStyle} marginVertical="$1" space="$3">
+        <Image
+          alignSelf="center"
+          source={{
+            uri: profile?.avatar_url,
+            height: 150,
+            width: 150,
+          }}
+        />
+        <YStack>
+          <Text text="Profile" />
+        </YStack>
       </YStack>
     </Nav>
   )
