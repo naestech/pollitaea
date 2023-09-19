29d80343f0abec855a2c007a768ffe1b681c3f9b
Samuel Adetunji <guysnacho2@gmail.com>
2023-09-19 02:46:54 -0500

    Add db fields to app state

--- Pollitaea/app/models/User.ts
+++ Pollitaea/app/models/User.ts
@@ -1,7 +1,9 @@
 import { User as SupaUser } from "@supabase/supabase-js"
+import { NavList } from "app/components"
+import { Profile } from "app/config/schema"
+import { supabase } from "app/utils/supabaseClient"
 import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { withSetPropAction } from "./helpers/withSetPropAction"
-import { supabase } from "app/utils/supabaseClient"
 
 /**
  * User model for managing current user or viewed users
@@ -13,8 +15,13 @@ export const UserModel = types
     id: types.maybe(types.string),
     username: types.maybe(types.string),
     email: types.maybe(types.string),
+    avatar_url: types.maybe(types.string),
+    external_url: types.maybe(types.string),
+    full_name: types.maybe(types.string),
+    location: types.maybe(types.string),
+    role: types.maybe(types.string),
+    tag: types.maybe(types.string),
     createdAt: types.maybe(types.string),
-    profilePic: types.maybe(types.string),
   })
   .actions(withSetPropAction)
   .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
@@ -25,7 +32,12 @@ export const UserModel = types
       self.username = username
       self.email = "user@pollitaea.app"
       self.createdAt = Date.now().toLocaleString()
-      self.profilePic = "https://placehold.co/150.png"
+      self.avatar_url = "https://placehold.co/150.png"
+      self.external_url = undefined
+      self.full_name = "Jane Doe"
+      self.location = "District of Columbia"
+      self.role = "STOCK"
+      self.tag = "Local voter"
     },
     login(supaUser: SupaUser) {
       self.authenticated = true
@@ -34,7 +46,7 @@ export const UserModel = types
       self.email = supaUser.email
       self.createdAt = supaUser.created_at
     },
-    logout() {
+    logout(navigation: NavList) {
       // Actually log out if this isn't a trial
       if (self.authenticated) {
         supabase.auth
@@ -42,6 +54,7 @@ export const UserModel = types
           .then((res) => {
             console.log(res)
             console.log("successful logout")
+            navigation.navigate("Welcome")
           })
           .catch((err) => {
             console.log(err)
@@ -53,10 +66,25 @@ export const UserModel = types
       self.username = undefined
       self.email = undefined
       self.createdAt = undefined
-      self.profilePic = undefined
+      self.avatar_url = undefined
+      self.external_url = undefined
+      self.full_name = undefined
+      self.location = undefined
+      self.role = undefined
+      self.tag = undefined
     },
-    setProfilePic(url: string) {
-      self.profilePic = url
+    hydrateProfile(profile: Profile, user: SupaUser) {
+      self.authenticated = true
+      self.id = user.id
+      self.username = profile.username
+      self.email = user.email
+      self.createdAt = user.created_at
+      self.avatar_url = profile.avatar_url
+      self.external_url = profile.external_url
+      self.full_name = profile.full_name
+      self.location = profile.location
+      self.role = profile.role
+      self.tag = profile.tag
     },
   })) // eslint-disable-line @typescript-eslint/no-unused-vars
 
@@ -70,5 +98,10 @@ export const createUserDefaultModel = () =>
     username: undefined,
     email: undefined,
     createdAt: undefined,
-    profilePic: undefined,
+    avatar_url: undefined,
+    external_url: undefined,
+    full_name: undefined,
+    location: undefined,
+    role: undefined,
+    tag: undefined,
   })
