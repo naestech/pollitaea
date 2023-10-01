d4c14d04a1eb28dc32b8c1e4d719511dfc7eb9f0
Samuel Adetunji <guysnacho2@gmail.com>
2023-10-01 01:48:59 -0500

    Add profile page reload

--- Pollitaea/app/components/Nav.tsx
+++ Pollitaea/app/components/Nav.tsx
@@ -154,7 +154,13 @@ export const Nav = observer(function Nav({ children, navigation, route }: NavPro
             <Tabs.Tab
               flex={1}
               value="Profile"
-              onPress={() => (route.name !== "Profile" ? navigation.replace("Profile") : undefined)}
+              onPress={() =>
+                route.name !== "Profile"
+                  ? navigation.replace("Profile")
+                  : route.params?.userId
+                  ? navigation.replace("Profile")
+                  : undefined
+              }
             >
               <User2 />
             </Tabs.Tab>
